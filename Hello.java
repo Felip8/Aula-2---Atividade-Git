00cf4e429963953b19fb8c576d2a468c4a035fa4
Felip8 <valdivinobiava496@gmail.com>
2025-03-06 13:10:29 -0300

    adicionado o metodo holaquetal

--- Hello.java
+++ Hello.java
@@ -2,10 +2,15 @@ public class Hello {
     public static void main(String [] args){
         System.out.println("Hello World");
         olamundo();
+        holaquetal();
     }
 
     public static void olamundo(){
         System.out.println("OLá mundo");
+    }
+
+    public static void holaquetal(){
+        System.out.println("Hola que tal");
     }
 
 }
